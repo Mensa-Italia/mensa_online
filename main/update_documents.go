305cd48003b3c0c1881cc76dd3df17b5a243c57a
Sipioteo <matteo@sipio.it>
2025-03-12 01:10:17 +0100

    Moved AI elaboration into another table

--- main/update_documents.go
+++ main/update_documents.go
@@ -75,8 +75,16 @@ func UpdateDocuments(document map[string]any) string {
 		newDocument.Set("published", document["date"].(time.Time))
 	}
 	newDocument.Set("file", document["file"].(*filesystem.File))
-	newDocument.Set("file_data", document["resume"].(string))
 	newDocument.Set("uploaded_by", "5366")
+	_ = app.Save(newDocument)
+
+	collectionElaborated, _ := app.FindCollectionByNameOrId("documents_elaborated")
+	recordElaborated := core.NewRecord(collectionElaborated)
+	recordElaborated.Set("document", newDocument.Id)
+	recordElaborated.Set("ia_resume", document["resume"].(string))
+	_ = app.Save(recordElaborated)
+
+	newDocument.Set("elaborated", recordElaborated.Id)
 	_ = app.Save(newDocument)
 	return newDocument.Id
 }
