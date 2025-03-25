feeb2c717caf163b048f6deed79a711e6dd36e63
Sipioteo <matteo@sipio.it>
2025-03-25 23:51:41 +0100

    Added payment to api

--- main/api/cs/example.go
+++ main/api/cs/example.go
@@ -6,10 +6,11 @@ func example(e *core.RequestEvent) error {
 	var bodyBytes []byte
 	_, _ = e.Request.Body.Read(bodyBytes)
 
-	e.JSON(200, map[string]interface{}{
+	return e.JSON(200, map[string]interface{}{
 		"Method": e.Request.Method,
 		"URL":    e.Request.URL.String(),
 		"Query":  e.Request.URL.Query(),
 		"Body":   string(bodyBytes),
 	})
+
 }
