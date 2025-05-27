61b70f0da6f322d9941d895e82914ac4941a98b7
Sipioteo <matteo@sipio.it>
2025-05-27 20:31:17 +0200

    Connected the members registry with the users table

---
+++ main/hooks/force_stamp.go
@@ -0,0 +1,24 @@
+package hooks
+
+import "github.com/pocketbase/pocketbase/core"
+
+func ForceStampGen(e *core.RequestEvent) error {
+	eventId := e.Request.PathValue("id")
+
+	if eventId == "" {
+		return e.Error(400, "Event ID is required", nil)
+	}
+
+	record, err := e.App.FindRecordById("events", eventId)
+	if err != nil {
+		return err
+	}
+
+	bytesImage := createEventStamp(e.App, record)
+
+	if bytesImage == nil {
+		return e.Error(500, "Failed to create event stamp", nil)
+	}
+
+	return e.Blob(200, "image/png", bytesImage)
+}
