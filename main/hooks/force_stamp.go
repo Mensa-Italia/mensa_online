72f6cdfa219abf6cfef2b2c63e36f31491837b80
Sipioteo <matteo@sipio.it>
2025-05-27 20:41:59 +0200

    Connected the members registry with the users table

--- main/hooks/force_stamp.go
+++ main/hooks/force_stamp.go
@@ -3,6 +3,7 @@ package hooks
 import "github.com/pocketbase/pocketbase/core"
 
 func ForceStampGen(e *core.RequestEvent) error {
+	return e.Error(400, "This endpoint is not available", nil)
 	eventId := e.Request.PathValue("id")
 
 	if eventId == "" {
