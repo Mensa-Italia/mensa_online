cee45dfd00bbb6da81eed1f032cc66875b6531f5
Sipioteo <matteo@sipio.it>
2025-07-27 16:08:39 +0200

    Aggiunta area/zona per i membri

---
+++ tools/spatial/check_province_from_state.go
@@ -0,0 +1,22 @@
+package spatial
+
+import (
+	"github.com/tidwall/gjson"
+	"os"
+	"strings"
+)
+
+func CheckProvinceFromState(state string) string {
+	dataRead, err := os.ReadFile("pb_public/province.json")
+	if err != nil {
+		return "NaN"
+	}
+	province := gjson.ParseBytes(dataRead)
+
+	for _, item := range province.Array() {
+		if strings.ToLower(item.Get("nome").String()) == strings.ToLower(state) {
+			return item.Get("regione").String()
+		}
+	}
+	return "NaN"
+}
