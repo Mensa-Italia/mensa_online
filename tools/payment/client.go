0a8f867c50d3a3916ffbbfd9af87796f4e4f9534
Sipioteo <matteo@sipio.it>
2025-09-26 16:49:36 +0200

    Fix customers stripe

--- tools/payment/client.go
+++ tools/payment/client.go
@@ -23,3 +23,30 @@ func NewCustomer(userId, name, email string) (*stripe.Customer, error) {
 	}
 	return GetClient().Customers.New(customerParams)
 }
+
+func ExistsCustomer(stripeCustomerId string) (bool, error) {
+	_, err := GetClient().Customers.Get(stripeCustomerId, nil)
+	if err != nil {
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			if stripeErr.Code == stripe.ErrorCodeResourceMissing {
+				return false, nil
+			}
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func NewCustomerIfNotExists(userId, name, email string) (*stripe.Customer, error) {
+	customerParams := &stripe.CustomerListParams{
+		Email: &email,
+	}
+	i := GetClient().Customers.List(customerParams)
+	for i.Next() {
+		c := i.Customer()
+		if c.Metadata["user_id"] == userId {
+			return c, nil
+		}
+	}
+	return NewCustomer(userId, name, email)
+}
