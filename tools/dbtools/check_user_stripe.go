4c0e67c3e4021abed2575a5bcdab3ecc3fdb981f
Sipioteo <matteo@sipio.it>
2025-09-26 17:08:39 +0200

    Fix customers stripe

--- tools/dbtools/check_user_stripe.go
+++ tools/dbtools/check_user_stripe.go
@@ -1,28 +1,36 @@
 package dbtools
 
 import (
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase/core"
 	"mensadb/tools/payment"
 )
 
 func CheckUserStripeAccount(app core.App) {
-	records, err := app.FindAllRecords("users_secrets", nil)
-	if err != nil {
-		return
-	}
 
-	for _, record := range records {
-		if record.GetString("key") == "stripe_customer_id" {
-			user, _ := app.FindRecordById("users", record.GetString("user"), nil)
-			customer, _ := payment.NewCustomerIfNotExists(
-				user.Id,
-				user.GetString("name"),
-				user.GetString("email"),
-			)
-			if customer != nil && customer.ID != record.GetString("value") {
+	users, _ := app.FindAllRecords("users", nil)
+
+	for _, user := range users {
+		customer, _ := payment.NewCustomerIfNotExists(
+			user.Id,
+			user.GetString("name"),
+			user.GetString("email"),
+		)
+		if customer != nil {
+			filter, err := app.FindFirstRecordByFilter("users_secrets", "user = ? AND key = ? AND value = ?", dbx.Params{
+				"user":  user.Id,
+				"key":   "stripe_customer_id",
+				"value": customer.ID,
+			})
+			if err != nil || filter == nil {
+				collection, _ := app.FindCollectionByNameOrId("users_secrets")
+				record := core.NewRecord(collection)
+				record.Set("user", user.Id)
+				record.Set("key", "stripe_customer_id")
 				record.Set("value", customer.ID)
 				_ = app.Save(record)
 			}
 		}
 	}
+
 }
