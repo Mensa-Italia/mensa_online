768b04811db2de3e2b3533154caaf010cf89eeec
Sipioteo <matteo@sipio.it>
2025-09-26 17:09:51 +0200

    Fix customers stripe

--- tools/dbtools/check_user_stripe.go
+++ tools/dbtools/check_user_stripe.go
@@ -11,18 +11,17 @@ func CheckUserStripeAccount(app core.App) {
 	users, _ := app.FindAllRecords("users", nil)
 
 	for _, user := range users {
-		customer, _ := payment.NewCustomerIfNotExists(
-			user.Id,
-			user.GetString("name"),
-			user.GetString("email"),
-		)
-		if customer != nil {
-			filter, err := app.FindFirstRecordByFilter("users_secrets", "user = ? AND key = ? AND value = ?", dbx.Params{
-				"user":  user.Id,
-				"key":   "stripe_customer_id",
-				"value": customer.ID,
-			})
-			if err != nil || filter == nil {
+		filter, err := app.FindFirstRecordByFilter("users_secrets", "user = ? AND key = ?", dbx.Params{
+			"user": user.Id,
+			"key":  "stripe_customer_id",
+		})
+		if err != nil || filter == nil {
+			customer, _ := payment.NewCustomerIfNotExists(
+				user.Id,
+				user.GetString("name"),
+				user.GetString("email"),
+			)
+			if customer != nil {
 				collection, _ := app.FindCollectionByNameOrId("users_secrets")
 				record := core.NewRecord(collection)
 				record.Set("user", user.Id)
