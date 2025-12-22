470160a4bc9250454ef610fcbf96eecd6e323978
Sipioteo <matteo@sipio.it>
2025-12-22 16:23:28 +0100

    Minor fixes

--- main/api/cs/members_snapshots.go
+++ main/api/cs/members_snapshots.go
@@ -1,10 +1,13 @@
 package cs
 
 import (
+	"encoding/json"
 	"mensadb/tools/cdnfiles"
 	"strings"
 
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/tidwall/gjson"
 )
 
 func MembersSnapshotsHandler(e *core.RequestEvent) error {
@@ -28,6 +31,7 @@ func MembersSnapshotsHandler(e *core.RequestEvent) error {
 	}
 
 	for _, key := range listOfStuffs {
+		key := strings.ReplaceAll(key, "snapshot_members/", "")
 		timestamp := strings.ReplaceAll(key, ".json.gz", "")
 		snapshotInfo.History = append(snapshotInfo.History, SnapshotInfoElem{
 			Timestamp: timestamp,
@@ -35,9 +39,60 @@ func MembersSnapshotsHandler(e *core.RequestEvent) error {
 		})
 	}
 
-	return e.JSON(200, listOfStuffs)
+	return e.JSON(200, snapshotInfo)
 }
 
 func MemberSnapshotByKeyHandler(e *core.RequestEvent) error {
-	return e.String(200, "Not implemented yet")
+	s3settings := e.App.Settings().S3
+	key := e.Request.PathValue("key")
+	hideNotActive := e.Request.URL.Query().Get("hideNotActive")
+	hideNowNotActive := e.Request.URL.Query().Get("hideNowNotActive")
+
+	fileContent, err := cdnfiles.RetrieveFileFromS3(e.App, s3settings.Bucket, "snapshot_members/"+key)
+
+	if err != nil {
+		return err
+	}
+
+	decompressedContent, err := cdnfiles.GzipDecompressBytes(fileContent)
+	if err != nil {
+		return err
+	}
+	filteredContent, err := filterMembersSnapshot(e.App, decompressedContent, hideNotActive != "", hideNowNotActive != "")
+	if err != nil {
+		return err
+	}
+
+	var jsonData interface{}
+	err = json.Unmarshal([]byte(filteredContent), &jsonData)
+
+	return e.JSON(200, jsonData)
+}
+
+func filterMembersSnapshot(app core.App, snapshotData []byte, hideNotActive bool, hideNowNotActive bool) (string, error) {
+	snapshotElems := gjson.ParseBytes(snapshotData)
+	records, _ := app.FindAllRecords("members_registry", dbx.NewExp("is_active = true"))
+	activeMemberIds := map[string]bool{}
+	for _, record := range records {
+		activeMemberIds[record.Id] = true
+	}
+
+	snapshotElems.ForEach(func(key, value gjson.Result) bool {
+		memberId := value.Get("id").String()
+		isActive := value.Get("is_active").Bool()
+		if hideNotActive && !isActive {
+			return false // remove this element
+		}
+		if hideNowNotActive {
+			if _, exists := activeMemberIds[memberId]; !exists {
+				return false // remove this element
+			}
+		}
+		return true // keep this element
+	})
+
+	//snapshotElems.String() to bytes
+
+	return snapshotElems.String(), nil
+
 }
