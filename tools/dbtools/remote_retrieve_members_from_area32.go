36f5290a718f3cabae3d951c3ea4bb3492a03255
Sipioteo <matteo@sipio.it>
2025-12-22 13:12:24 +0100

    Fixed Cron Time for expensive tasks

--- tools/dbtools/remote_retrieve_members_from_area32.go
+++ tools/dbtools/remote_retrieve_members_from_area32.go
@@ -111,9 +111,9 @@ func UpdateMembers(app core.App, member map[string]any) string {
 		elems := gjson.ParseBytes(marshal)
 		newRecord.Set("original_mail", strings.ToLower(strings.TrimSpace(strings.ReplaceAll(elems.Get("E-mail:").String(), "mailto:", ""))))
 		alias := importers.RetrieveAliasFromMail(strings.ToLower(strings.TrimSpace(strings.ReplaceAll(elems.Get("E-mail:").String(), "mailto:", ""))))
-		newRecord.Set("alias_mail", alias+"@mensa.it")
+		newRecord.Set("alias_mail", alias)
 
-		member["deepData"].(map[string]string)["E-mail:"] = "mailto:" + alias + "@mensa.it"
+		member["deepData"].(map[string]string)["E-mail:"] = "mailto:" + alias
 		marshal, err := json.Marshal(member["deepData"])
 		if err == nil {
 			newRecord.Set("full_data", marshal)
