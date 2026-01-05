7cb51b42df30ed8354213c78c5081a266226c29b
Sipioteo <matteo@sipio.it>
2026-01-06 00:06:49 +0100

    Changed api endpoint for tolgee so that use the internal service

--- main/crons/crontasks.go
+++ main/crons/crontasks.go
@@ -1,9 +1,10 @@
-package dbtools
+package crons
 
 import (
 	"fmt"
 	"mensadb/importers"
 	"mensadb/tolgee"
+	"mensadb/tools/dbtools"
 	"mensadb/tools/env"
 	"mensadb/tools/zincsearch"
 
@@ -12,28 +13,28 @@ import (
 
 func CronTasks(app core.App) {
 	app.Cron().MustAdd("Update remote addons", "1 3 * * *", func() {
-		RemoteUpdateAddons(app)
+		dbtools.RemoteUpdateAddons(app)
 	})
 
 	app.Cron().MustAdd("Update states managers powers", "1 3 * * *", func() {
 		importers.GetFullMailList()
-		RefreshUserStatesManagersPowers(app)
+		dbtools.RefreshUserStatesManagersPowers(app)
 		app.Logger().Info(
 			fmt.Sprintf("[CRON] Updated the powers of all the users based on the segretari list"),
 		)
 	})
 
 	app.Cron().MustAdd("Reload Tolgee Translations", "1 3 * * *", func() {
-		tolgee.Load(env.GetTolgeeKey())
+		tolgee.Load(env.GetTolgeeKey(), app)
 	})
 
 	app.Cron().MustAdd("Update documents data", "0 6-20 * * *", func() {
-		RemoteRetrieveDocumentsFromArea32(app)
+		dbtools.RemoteRetrieveDocumentsFromArea32(app)
 	})
 
 	app.Cron().MustAdd("Update registry data", "30 0,3,6,9,12,15,18,21 * * *", func() {
 		importers.GetFullMailList()
-		RemoteRetrieveMembersFromArea32(app)
+		dbtools.RemoteRetrieveMembersFromArea32(app)
 	})
 
 	app.Cron().MustAdd("Upload file to zinc", "0 0,3 * * *", func() {
@@ -41,10 +42,10 @@ func CronTasks(app core.App) {
 	})
 
 	app.Cron().MustAdd("CheckUserStripeAccount", "0 */6 * * *", func() {
-		CheckUserStripeAccount(app)
+		dbtools.CheckUserStripeAccount(app)
 	})
 
 	app.Cron().MustAdd("Snapshot Members Registry", "0 0 * * *", func() {
-		SnapshotArea32Members(app)
+		dbtools.SnapshotArea32Members(app)
 	})
 }
