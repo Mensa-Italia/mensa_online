e00394b06d5e300b7f1d24127fc09c46883044dd
Sipioteo <matteo@sipio.it>
2026-05-14 21:30:56 +0200

    fix(migration): bypass validator per cambio target relation org_chart.user
    Il validator di schema PocketBase blocca esplicitamente il cambio di
    collectionId su un relation field esistente ("The relation collection
    cannot be changed"). Soluzione: aggiornare direttamente la riga
    _collections.fields via SQL (json_set sul path \$[N].collectionId) e
    ricaricare la cache delle collection con ReloadCachedCollections().
    
    I valori esistenti (id socio) restano intatti. Forward e rollback
    identici eccetto target (members_registry vs users).

--- migrations/1778786000_org_chart_user_to_member.go
+++ migrations/1778786000_org_chart_user_to_member.go
@@ -3,6 +3,7 @@ package migrations
 import (
 	"fmt"
 
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase/core"
 	m "github.com/pocketbase/pocketbase/migrations"
 )
@@ -11,47 +12,54 @@ import (
 // "members_registry". I valori esistenti (id socio = id tessera) restano
 // validi perche` entrambe le collection sono chiavate sull'id tessera.
 // In-place: nessuna perdita di dati.
+//
+// Implementazione: PocketBase il validator di schema BLOCCA il cambio del
+// collectionId su un relation field esistente (errore "The relation
+// collection cannot be changed."). Aggiriamo aggiornando direttamente
+// _collections.fields via SQL e ricaricando la cache.
 func init() {
 	m.Register(func(app core.App) error {
-		col, err := app.FindCollectionByNameOrId("org_chart_members")
-		if err != nil {
-			return err
-		}
-		members, err := app.FindCollectionByNameOrId("members_registry")
-		if err != nil {
-			return fmt.Errorf("members_registry not found: %w", err)
-		}
-
-		f := col.Fields.GetByName("user")
-		if f == nil {
-			return fmt.Errorf("field user not found on org_chart_members")
-		}
-		rf, ok := f.(*core.RelationField)
-		if !ok {
-			return fmt.Errorf("field user is not a relation field")
-		}
-
-		rf.CollectionId = members.Id
-		return app.Save(col)
+		return swapRelationTarget(app, "org_chart_members", "user", "members_registry")
 	}, func(app core.App) error {
-		// Rollback: ripuntare a users.
-		col, err := app.FindCollectionByNameOrId("org_chart_members")
-		if err != nil {
-			return nil
-		}
-		users, err := app.FindCollectionByNameOrId("users")
-		if err != nil {
-			return err
-		}
-		f := col.Fields.GetByName("user")
-		if f == nil {
-			return nil
-		}
-		rf, ok := f.(*core.RelationField)
-		if !ok {
-			return nil
-		}
-		rf.CollectionId = users.Id
-		return app.Save(col)
+		return swapRelationTarget(app, "org_chart_members", "user", "users")
 	})
+}
+
+func swapRelationTarget(app core.App, collection, fieldName, newTarget string) error {
+	col, err := app.FindCollectionByNameOrId(collection)
+	if err != nil {
+		return err
+	}
+	target, err := app.FindCollectionByNameOrId(newTarget)
+	if err != nil {
+		return fmt.Errorf("target collection %q not found: %w", newTarget, err)
+	}
+
+	idx := -1
+	for i, f := range col.Fields {
+		if f.GetName() == fieldName {
+			idx = i
+			break
+		}
+	}
+	if idx == -1 {
+		return fmt.Errorf("field %q not found on %s", fieldName, collection)
+	}
+
+	// json_set sostituisce solo il valore di collectionId, lasciando intatti
+	// gli altri attributi del field.
+	path := fmt.Sprintf("$[%d].collectionId", idx)
+	_, err = app.DB().NewQuery(
+		"UPDATE _collections SET fields = json_set(fields, {:path}, {:newId}) WHERE id = {:colId}",
+	).Bind(dbx.Params{
+		"path":  path,
+		"newId": target.Id,
+		"colId": col.Id,
+	}).Execute()
+	if err != nil {
+		return fmt.Errorf("update _collections: %w", err)
+	}
+
+	app.ReloadCachedCollections()
+	return nil
 }
